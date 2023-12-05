dfefcec29efa4b09997f3450920221f7c18bec4e
Sofia Costa <152967284+salmons-handier@users.noreply.github.com>
2023-12-05 20:32:11 +0700

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,20 @@
+// Patient.java
+public class Patient {
+    private String name;
+    private int age;
+
+    public Patient(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Additional patient-related methods
+}
