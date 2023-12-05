25d4a2881916c85fc8a97caa2a4c582860856bb7
Sofia Costa <152967284+salmons-handier@users.noreply.github.com>
2023-12-05 20:32:35 +0700

    Create Appointment.java

---
+++ Appointment.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+// Appointment.java
+public class Appointment {
+    private Patient patient;
+    private Doctor doctor;
+    private Date date;
+
+    public Appointment(Patient patient, Doctor doctor, Date date) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    // Additional appointment-related methods
+}
